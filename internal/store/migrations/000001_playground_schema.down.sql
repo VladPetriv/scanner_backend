597d9b0ce894bcd0cae6f38ed9136f8932327e8c
Vlad Petriv <vladpetriv05@gmail.com>
2022-04-26 16:15:11 +0300

    update migrations and add user to id

--- internal/store/migrations/000001_playground_schema.down.sql
+++ internal/store/migrations/000001_playground_schema.down.sql
@@ -1,3 +1,4 @@
 DROP TABLE replie;
 DROP TABLE message;
+DROP TABLE tg_user;
 DROP TABLE channel;
--- internal/store/migrations/000001_playground_schema.up.sql
+++ internal/store/migrations/000001_playground_schema.up.sql
@@ -3,16 +3,27 @@ CREATE TABLE channel (
   name VARCHAR(255)
 );
 
+CREATE TABLE tg_user (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(255) UNIQUE NOT NULL,
+  fullname VARCHAR(255) UNIQUE NOT NULL,
+  photourl TEXT NOT NULL 
+);
+
 CREATE TABLE message (
   id SERIAL PRIMARY KEY,
   channel_id INT NOT NULL,
+  user_id INT NOT NULL,
   title TEXT,
-  CONSTRAINT fk_channel FOREIGN KEY(channel_id) REFERENCES channel(id)
+  CONSTRAINT fk_channel FOREIGN KEY(channel_id) REFERENCES channel(id),
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES tg_user(id)
 );
 
 CREATE TABLE replie (
   id SERIAL PRIMARY KEY,
   message_id INT NOT NULL,
+  user_id INT NOT NULL,
   title TEXT,
-  CONSTRAINT fk_message FOREIGN KEY(message_id) REFERENCES message(id)
+  CONSTRAINT fk_message FOREIGN KEY(message_id) REFERENCES message(id),
+  CONSTRAINT fk_user FOREIGN KEY(user_id) REFERENCES tg_user(id)
 );
