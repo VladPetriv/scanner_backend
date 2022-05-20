1667e02d1e9de13920ad8d8a0bd8658c4b51ec76
Vlad Petriv <vladpetriv05@gmail.com>
2022-05-20 18:36:44 +0300

    test: update mocks because added new message method

--- internal/store/mocks/MessageRepo.go
+++ internal/store/mocks/MessageRepo.go
@@ -170,3 +170,24 @@ func (_m *MessageRepo) GetMessages() ([]model.Message, error) {
 
 	return r0, r1
 }
+
+// GetMessagesLengthByChannelID provides a mock function with given fields: ID
+func (_m *MessageRepo) GetMessagesLengthByChannelID(ID int) (int, error) {
+	ret := _m.Called(ID)
+
+	var r0 int
+	if rf, ok := ret.Get(0).(func(int) int); ok {
+		r0 = rf(ID)
+	} else {
+		r0 = ret.Get(0).(int)
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(int) error); ok {
+		r1 = rf(ID)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
