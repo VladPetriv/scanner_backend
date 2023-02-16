907bcb5803b7e2041f8db0145270c937f7d8bb05
Vlad Petriv <vladpetriv05@gmail.com>
2023-02-16 20:18:04 +0100

    refactor: logs, error messages

--- internal/service/user.go
+++ internal/service/user.go
@@ -28,81 +28,50 @@ func NewUserService(store *store.Store, logger *logger.Logger, message MessageSe
 func (s userService) CreateUser(user *model.User) (int, error) {
 	logger := s.logger
 
-	candidate, err := s.GetUserByUsername(user.Username)
+	candidate, err := s.store.User.GetUserByUsername(user.Username)
 	if err != nil {
-		if !errors.Is(err, ErrUserNotFound) {
-			logger.Error().Err(err).Msg("get user by username")
-			return 0, fmt.Errorf("[CreateUser] get user by username error: %w", err)
-		}
+		logger.Error().Err(err).Msg("get user by username")
+		return 0, fmt.Errorf("get user by username from db: %w", err)
 	}
 	if candidate != nil {
-		logger.Info().Msg("user found")
+		logger.Info().Msg("user found, don't create new user")
 		return candidate.ID, nil
 	}
 
 	id, err := s.store.User.CreateUser(user)
 	if err != nil {
 		logger.Error().Err(err).Msg("create user")
-		return id, fmt.Errorf("create user error: %w", err)
+		return id, fmt.Errorf("create user in db: %w", err)
 	}
 
 	logger.Info().Int("userID", id).Msg("user successfully created")
 	return id, nil
 }
 
-func (s userService) GetUserByUsername(username string) (*model.User, error) {
-	logger := s.logger
-
-	user, err := s.store.User.GetUserByUsername(username)
-	if err != nil {
-		logger.Error().Err(err).Msg("get user by username")
-		return nil, fmt.Errorf("get user by username: %w", err)
-	}
-
-	if user == nil {
-		logger.Info().Msg("user not found")
-		return nil, ErrUserNotFound
-	}
-
-	logger.Info().Interface("user", user).Msg("successfully got user by username")
-	return user, nil
-}
-
-func (s userService) GetUserByID(id int) (*model.User, error) {
-	logger := s.logger
-
-	user, err := s.store.User.GetUserByID(id)
-	if err != nil {
-		logger.Error().Err(err).Msg("get user by id")
-		return nil, fmt.Errorf("get user by id: %w", err)
-	}
-
-	if user == nil {
-		logger.Info().Msg("user not found")
-		return nil, ErrUserNotFound
-	}
-
-	logger.Info().Interface("user", user).Msg("successfully got user by id")
-	return user, nil
-}
-
 func (s userService) ProcessUserPage(userID int) (*LoadUserOutput, error) {
 	logger := s.logger
 
-	user, err := s.GetUserByID(userID)
+	user, err := s.store.User.GetUserByID(userID)
 	if err != nil {
-		if !errors.Is(err, ErrUserNotFound) {
-			logger.Error().Err(err).Msg("get user by id")
-			return nil, fmt.Errorf("[ProcessUserPage] get user by id error: %w", err)
-		}
+		logger.Error().Err(err).Msg("get user by id")
+		return nil, fmt.Errorf("get user by id from db: %w", err)
+	}
+	if user == nil {
+		logger.Info().Msg("user not found")
+		return nil, ErrUserNotFound
 	}
 
 	messages, err := s.message.GetFullMessagesByUserID(user.ID)
 	if err != nil {
 		if errors.Is(err, ErrMessagesNotFound) {
-			logger.Error().Err(err).Msg("get messages by user id")
-			return nil, fmt.Errorf("[ProcessUserPage] get messages by user id error: %w", err)
+			logger.Info().Int("user id", user.ID).Msg("messages by user id not found")
+			return &LoadUserOutput{
+				TgUser: user,
+			}, nil
 		}
+
+		logger.Error().Err(err).Msg("get messages by user id")
+		return nil, fmt.Errorf("[ProcessUserPage]: %w", err)
 	}
 
 	return &LoadUserOutput{
