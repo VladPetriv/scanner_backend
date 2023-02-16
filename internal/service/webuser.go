608c958594e818e4c3f9918648a0bf73dee1a0d5
Vlad Petriv <vladpetriv05@gmail.com>
2023-02-16 20:31:43 +0100

    refactor: remove unused method, change log, errors style

--- internal/service/webuser.go
+++ internal/service/webuser.go
@@ -28,29 +28,11 @@ func (s webUserService) CreateWebUser(user *model.WebUser) error {
 	err := s.store.WebUser.CreateWebUser(user)
 	if err != nil {
 		logger.Error().Err(err).Msg("create web user")
-		return fmt.Errorf("create web user error: %w", err)
+		return fmt.Errorf("create web user in db: %w", err)
 	}
 
 	logger.Info().Msg("web user successfully created")
 	return nil
-}
-
-func (s webUserService) GetWebUserByID(userID int) (*model.WebUser, error) {
-	logger := s.logger
-
-	user, err := s.store.WebUser.GetWebUserByID(userID)
-	if err != nil {
-		logger.Error().Err(err).Msg("get web user by id")
-		return nil, fmt.Errorf("get web user by id error: %w", err)
-	}
-
-	if user == nil {
-		logger.Info().Msg("web user not found")
-		return nil, ErrWebUserNotFound
-	}
-
-	logger.Info().Interface("web user", user).Msg("web user successfully found")
-	return user, nil
 }
 
 func (s webUserService) GetWebUserByEmail(email string) (*model.WebUser, error) {
@@ -59,14 +41,13 @@ func (s webUserService) GetWebUserByEmail(email string) (*model.WebUser, error) 
 	user, err := s.store.WebUser.GetWebUserByEmail(email)
 	if err != nil {
 		logger.Error().Err(err).Msg("get web user by email")
-		return nil, fmt.Errorf("get web user by email error: %w", err)
+		return nil, fmt.Errorf("get web user by email from db: %w", err)
 	}
-
 	if user == nil {
-		logger.Info().Msg("web user not found")
+		logger.Info().Str("user email", email).Msg("web user by email not found")
 		return nil, ErrWebUserNotFound
 	}
 
-	logger.Info().Interface("web user", user).Msg("web user successfully found")
+	logger.Info().Interface("web user", user).Msg("successfully got web user")
 	return user, nil
 }
